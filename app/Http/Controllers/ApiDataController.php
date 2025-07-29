32455527f08fe21e2a0e9e2988bec231261d145b
rshiggin <rshiggin@users.noreply.github.com>
2025-07-29 18:15:43 -0400

    Prep for creating store and fetch service

--- app/Http/Controllers/ApiDataController.php
+++ app/Http/Controllers/ApiDataController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AccessToken;
 use Illuminate\Support\Facades\Http;
 
 class ApiDataController extends Controller {
     // fetch ORCID ID
         public function show() {
-                
-                $username = 'username';
-                $password = 'password';
-                $currentUser = 'currentUser';
+
+                $username = env('BASIC_AUTH_USER');
+                $password = env('BASIC_AUTH_PASS');
+                $currentUser = env('USERNAME');
 
         $credentials = base64_encode("{$username}:{$password}");
 
@@ -24,15 +25,11 @@ class ApiDataController extends Controller {
         $response = Http::withHeaders($commonHeaders)
             ->get("https://unt.identity.iu.edu/registry/api/co/3/core/v1/people/{$currentUser}");
 
-        // debug response
-        // dump($getOrcid);
-
         $data = $response->json(); 
 
         $userData = null;
-
         foreach ($data['OrgIdentity'] as $org) {
-            if (($org['meta']['actor_identifier'] ?? '') === 'rshiggin') {
+            if (($org['meta']['actor_identifier'] ?? '') === $currentUser) {
         
                 foreach ($org['Identifier'] as $id) {
                     if (($id['type'] ?? '') === 'orcid') {
@@ -57,11 +54,34 @@ class ApiDataController extends Controller {
                 'coid'  => 3,
             ]);
 
-        // debug response
-        // dump($getToken);
+      	$tokenData = $getToken->successful() ? $getToken->json() : ['error' => 'API returned: ' . $getToken->status()];
+/*
+        $validated = $request->validate([
+            'orcid_id'      => 'required|string|size:19|unique:orcid_tokens,orcid_id',
+            'scopes'        => 'nullable|string',
+            'access_token'  => 'required|string',
+            'id_token'      => 'required|string',
+            'refresh_token' => 'required|string',
+        ]);
 
-       $tokenData = $getToken->successful() ? $getToken->json() : ['error' => 'API returned: ' . $getToken->status()];
+        $token = AccessToken::create($validated);
+ */
+        // Use orcid_id to make an API request
+/*        $orcidId = $token->orcid_id;
+        $accessToken = $token->access_token;
 
+        $orcidProfile = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => "Bearer {$accessToken}",
+        ])->get("https://api.sandbox.orcid.org/v3.0/{$orcidId}/summary");
+
+        if ($orcidProfile->successful()) {
+            return orcidProfile()->json([
+                'message' => 'Token saved and ORCID data retrieved.',
+                'orcidProfile' => $response->json(),
+            ]);
+        }
+ */
         // sends data to view template 'comanage.blade.php'
         return view('comanage', compact('userData', 'tokenData'));
     }
